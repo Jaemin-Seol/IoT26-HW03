b94495fce238d295b1afc61b57b3950f484f7b18
Seol Jaemin <100254362+Jaemin-Seol@users.noreply.github.com>
2026-05-04 21:47:15 +0900

    Implement IoT26-HW03_SeolJaemin.py
    HW03_Seol Jaemin implemented.
    
    README updated.
    .gitignore updated.

---
+++ IoT26-HW03_SeolJaemin.py
@@ -0,0 +1,37 @@
+# IoT26-HW03_SeolJaemin.py
+# 202234900 Seol Jaemin
+
+from gpiozero import Button, MotionSensor
+from picamera import PiCamera
+from time import sleep
+from signal import pause
+
+# create objects
+button = Button(2) # GPIO 2 = pin 3
+pir = MotionSensor(4) # GPIO = pin 7
+camera = PiCamera()
+
+# Start cam
+camera.rotation = 180 # Based on camera's physical orientation
+camera.start_preview()
+
+i = 0 # image name
+
+# function to stop camera and program
+def stop_cameara():
+    camera.stop_preview()
+    exit()
+
+# function to take photo
+def take_photo():
+    global i
+    i += 1
+    camera.capture(f'/home/pi/Desktop/image_{i}.jpg')
+    print("A photo has been taken")
+    sleep(10)
+
+# assign functions to button and motion sensor
+button.when_pressed = stop_cameara
+pir.when_motion = take_photo
+
+pause()
---
+++ README.md
@@ -0,0 +1,11 @@
+# IoT26-HW03 Team C (Cho Wooyoung, Seol Jaemin, Lim Youbin)
+
+Since this assignment covers the fundamentals, we each decided to implement/practice the code individually to ensure a clear understanding of the Raspberry Pi GPIO control. As a result, there are three variations of code, performing the same
+core task.
+
+
+## Result
+result image place holder (video of gif of RPI taking photos when motion is detected)
+
+
+## Screenshot of RPI IDE showing selected board & port

fd87e6876f8b30d1eeadfc7af5cc9c0b87ffe449
Seol Jaemin <100254362+Jaemin-Seol@users.noreply.github.com>
2026-05-06 22:55:57 +0900

    Update IoT26-HW03_SeolJaemin.py
    Update GPIO pins, image save path, and cooldown timer

--- IoT26-HW03_SeolJaemin.py
+++ IoT26-HW03_SeolJaemin.py
@@ -6,12 +6,12 @@ from picamera2 import Picamera2
 import libcamera
 from time import sleep
 from signal import pause
-import os.path
+import os
 
 # create objects
 # I swapped these two pins for convenience
 button = Button(4) # GPIO 4 = pin 7
-pir = MotionSensor(2) # GPIO 2 = pin 3
+pir = MotionSensor(17) # GPIO 17 = pin 11
 
 # Cam setup
 cam = Picamera2()
@@ -21,16 +21,21 @@ config = cam.create_still_configuration()
 config["transform"] = libcamera.Transform(vflip=1)
 cam.configure(config)
 
-# Start cam
+# Start cam and make folder
 cam.start()
+path = f'~/Desktop/images'
+path = os.path.expanduser(path)
+os.makedirs(path, exist_ok=True)
 
 i = 0 # image name
+
+print("Running...")
 
 # function to stop camera and program
 def stop_camera():
     cam.stop()
     print("Stopping...")
-    exit()
+    os._exit(0)
 
 # function to take photo
 def take_photo():
@@ -38,11 +43,16 @@ def take_photo():
     i += 1
 
     # set path
-    path = f'~/Desktop/image_{i}.jpg'
+    path = f'~/Desktop/images/image_{i}.jpg'
     path = os.path.expanduser(path)
     cam.capture_file(path)
     print("A photo has been taken")
-    sleep(5)
+    
+    # Cool down
+    for j in range(5, 0, -1):
+        print(f"Cooldown: {j}")
+        sleep(1)
+    print("Ready")
 
 # assign functions to button and motion sensor
 button.when_pressed = stop_camera
